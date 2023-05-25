b1fa2e69d452f8a786189a15dc375817c8572639
baitcode <batiyiv@gmail.com>
2023-05-26 00:54:06 +0100

    clean up

--- cmd/root.go
+++ cmd/root.go
@@ -40,20 +40,13 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "b64",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Cli tool to perform b64 encryption and decryption",
+	Long: `Cli tool written with Golang to perform b64 encryption and decryption. 
+	Nothing special, just a tool that is supposed to work the same on Linux, Max and Windows 
+	for the sake of writing simpler instructions for the software I make. 
+	Because, you know. You can't just use base64 encoding in shell in windows.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -62,13 +55,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.b64.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
